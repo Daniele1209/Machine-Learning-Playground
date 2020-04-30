840449bfbf97c5c06a48c93fab9428f074cef3ac
Mos Daniele <mdie2771@scs.ubbcluj.ro>
2020-05-01 01:44:28 +0300

    done project 1

--- Classifying Iris Species/main.py
+++ Classifying Iris Species/main.py
@@ -2,11 +2,13 @@ import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 import mglearn
-import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+
 # --- basic stuff ---
 
-# we load the Bunch object ftom sklearn.datasets
+# we load the Bunch object from sklearn.datasets
 iris_dataset = load_iris()
 
 # get the description of the dataset
@@ -42,3 +44,34 @@ print(y_test.shape)
 
 iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset.feature_names)
 pd.plotting.scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15), marker = 'o', hist_kwds = {'bins': 20}, s=60, alpha= .8, cmap = mglearn.cm3)
+plt.show()
+
+
+# --- k - nearest neighbors - build a machine learning model ---
+
+knn = KNeighborsClassifier(n_neighbors = 1)
+# call the fit method of the knn object
+knn.fit(X_train, y_train)
+
+
+# --- make predictions ---
+
+X_new = np.array([[5, 2.9, 1, 0.2]]) # data about a random iris that we assume we found
+print(X_new.shape)
+
+prediction = knn.predict(X_new)
+print(prediction) # <- this will be the prediction it came up with
+print(iris_dataset['target_names'][prediction]) # and this is the name
+
+
+# --- model evaluation ---
+
+y_pred = knn.predict(X_test)
+print(y_pred)
+
+print("Score test: {:.2f}".format(knn.score(X_test, y_test))) # this is the accuracy test result
+
+
+# INTRODUCTION PROJECT DONE :)
+# - Mos Daniele
+
