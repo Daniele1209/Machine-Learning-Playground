f138ccc53f5f9d34dd28b64c4e02bcef553ea101
Daniele <dany.mosh@yahoo.com>
2020-08-22 16:48:18 +0300

    split training set

--- House_price/main.py
+++ House_price/main.py
@@ -2,6 +2,8 @@ import os
 import tarfile
 import urllib
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 #fetch the data from the github repo
 
@@ -19,6 +21,25 @@ def fetch_housing_data(housing_url  = HOUSING_URL, housing_path = HOUSING_PATH):
 
 #load the data using pandas
 
-def lead_housing_data(housing_path = HOUSING_PATH):
+def load_housing_data(housing_path = HOUSING_PATH):
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
+
+fetch_housing_data()
+housing = load_housing_data()
+housing.head()
+
+
+housing.hist(bins = 50, figsize = (20, 15))
+plt.show()
+
+#splitting the training set so we can test the accuracy of the predictions later on
+
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+train_set, test_set = split_train_test(housing, 0.2)
